8cf2f17274ea573dccc9e6570a1910130f2217ce
KokoFibo <kokonacci@gmail.com>
2026-04-21 11:22:31 +0700

    tambah gtag

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -7,6 +7,18 @@
 </head>
 
 <body class="min-h-screen bg-white dark:bg-zinc-800">
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-BZ6XPXRVDJ"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-BZ6XPXRVDJ');
+    </script>
     <flux:header container class="border-b border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
         <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
 
