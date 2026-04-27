53a55b75b88b3a3d521739314ab2bae3a14ec2dc
KokoFibo <kokonacci@gmail.com>
2026-04-27 09:51:21 +0700

    tambah no follow

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -2,6 +2,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="darkt">
 
 <head>
+    <meta name="robots" content="noindex, nofollow">
+
     @include('partials.head')
 </head>
 
--- resources/views/components/layouts/auth/simple.blade.php
+++ resources/views/components/layouts/auth/simple.blade.php
@@ -2,6 +2,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
 
 <head>
+    <meta name="robots" content="noindex, nofollow">
+
     @include('partials.head')
 </head>
 
