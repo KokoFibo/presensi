fbf14cc6c7000733c9fc1aa05ca6fb4eb574d0bb
KokoFibo <kokonacci@gmail.com>
2026-05-04 19:39:03 +0700

    tambah gtag

--- resources/views/components/layouts/auth/simple.blade.php
+++ resources/views/components/layouts/auth/simple.blade.php
@@ -3,6 +3,18 @@
 
 <head>
     <meta name="robots" content="noindex, nofollow">
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-BZ6XPXRVDJ"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-BZ6XPXRVDJ');
+    </script>
 
     @include('partials.head')
 </head>
